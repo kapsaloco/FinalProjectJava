730bce8f11fa6ac455edb75f46a99eb568aa50b5
INT\Fernando.Giron <fernando.giron@solera.com>
2022-11-17 16:31:00 +0100

    Added CORS header

--- finalproject/src/main/java/com/solera/finalproject/controllers/PersonController.java
+++ finalproject/src/main/java/com/solera/finalproject/controllers/PersonController.java
@@ -4,9 +4,9 @@ import com.solera.finalproject.models.Person;
 import com.solera.finalproject.services.PersonService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,35 +22,53 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+
     @GetMapping
     public ResponseEntity<List<Person>> getAllPerson(){
-        return new ResponseEntity<>(
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Access-Control-Allow-Origin", "http://localhost:3000");
+        return new ResponseEntity<List<Person>>(
                 personService.getAllPerson(),
+                headers,
                 HttpStatus.OK);
     }
 
     @PostMapping
-    public Person postPerson(@RequestBody Person p) {
-        return personService.insertPerson(p);
+    public ResponseEntity<Person> postPerson(@RequestBody Person p) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Access-Control-Allow-Origin", "http://localhost:3000");
+        return new ResponseEntity<>(
+                personService.insertPerson(p),
+                headers,
+                HttpStatus.OK
+        );
     }
 
     @PutMapping
     public ResponseEntity<Person> updatePerson(@RequestBody Person p) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Access-Control-Allow-Origin", "http://localhost:3000");
+
         Person updatedPerson = personService.updatePerson(p);
         // Included a person without ID
         HttpStatus code = updatedPerson != null ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
 
         return new ResponseEntity<>(
                 updatedPerson,
+                headers,
                 code
         );
     }
 
     @DeleteMapping
     public ResponseEntity<String> deletePerson(@RequestBody Person p) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Access-Control-Allow-Origin", "http://localhost:3000");
+
         personService.deletePerson(p);
         return new ResponseEntity<>(
                 "Successfully deleted person with id: " + p.getId(),
+                headers,
                 HttpStatus.OK
         );
     }
