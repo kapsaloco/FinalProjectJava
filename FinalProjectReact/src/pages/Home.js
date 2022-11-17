1a609d8a9c9c7fef45b8789a35226617a7bcaab0
Kapsaloco <kapsalakosg@gmail.com>
2022-11-17 16:59:51 +0100

    Last Design Changes

--- FinalProjectReact/src/pages/Home.js
+++ FinalProjectReact/src/pages/Home.js
@@ -1,9 +1,16 @@
-//Main Home Page that uses Reusable Components
+import classes from "./Home.module.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      Hello
+    <div className={classes.outsideContainer}>
+      <div className={classes.innerContainer}>
+        <h1>Person Maker Pro Max</h1>
+        <p>The best Person Maker in the World</p>
+      </div>
+      <div className={classes.buttonContainer}>
+      <Link to="/api" style={{backgroundColor: "white", fontSize: "60px", textDecoration: "none", borderRadius: "4px", color: "black"}}>Try it now!</Link>
+      </div>
     </div>
   );
 }
