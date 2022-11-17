dbe43b717b1b08d03a0b8b0a658e1aa7d5af2372
Kapsaloco <kapsalakosg@gmail.com>
2022-11-17 15:37:58 +0100

    Implemented function for fetching all Persons + Design

--- FinalProjectReact/src/pages/PersonApi.js
+++ FinalProjectReact/src/pages/PersonApi.js
@@ -3,6 +3,7 @@ import { useRef } from "react";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import { useState, useEffect } from "react";
+import arrow from "./../images/arrow.png"
 
 function PersonApi() {
   const [valueNum, setValue] = useState();
@@ -32,11 +33,13 @@ function PersonApi() {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
-    }).then((data) => {
-      console.log("New Person Added");
-      // window.location.assign("http://localhost:3000/Login#s=1");
-    }).then(() => getAllEntries());
- 
+    })
+      .then((data) => {
+        console.log("New Person Added");
+        // window.location.assign("http://localhost:3000/Login#s=1");
+      })
+      .then(() => getAllEntries());
+
     //function to handle the inputted data (will be used on API)
     // props.onAddMeetup(meetupData);
   }
@@ -67,11 +70,11 @@ function PersonApi() {
           <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
               <label htmlFor="firstname">First Name</label>
-              <input type="text" required id="firstname" ref={firstnameRef} />
+              <input type="text" required id="firstname" ref={firstnameRef} maxLength="25"/>
             </div>
             <div className={classes.control}>
               <label htmlFor="lastname">Last Name</label>
-              <input type="text" required id="lastname" ref={lastnameRef} />
+              <input type="text" required id="lastname" ref={lastnameRef} maxLength="25"/>
             </div>
             <div className={classes.control}>
               <label htmlFor="address">Phone Number</label>
@@ -80,14 +83,16 @@ function PersonApi() {
                 value={valueNum}
                 ref={phonenumberRef}
                 onChange={setValue}
+                required
                 style={{ width: "71%", marginLeft: "48px" }}
+                maxLength="20"
               />
 
               {/* <input type="text" required id="number" ref={phonenumberRef} /> */}
             </div>
             <div className={classes.control}>
               <label htmlFor="description">E-mail</label>
-              <input type="text" required id="email" ref={emailRef} />
+              <input type="text" required id="email" ref={emailRef} maxLength="25"/>
             </div>
 
             <div className={classes.actions}>
@@ -97,13 +102,29 @@ function PersonApi() {
         </div>
       </div>
 
+      
       <div className={classes.column}></div>
+
+      <div className={classes.arrow}>
+        <img src={arrow}></img>
+      </div>
 
       <div className={classes.column}>
         <div className={classes.formContainerResults}>
-          {personData && personData.map((person) => (
-            <div>Hello</div>
-          ))}
+          {personData &&
+            personData.map((person) => (
+              <div className={classes.datarow}>
+                <div className={classes.datainnerow}>
+                  <div className={classes.datainnercolumn}>First Name: <b>{person.firstName}</b> </div>
+                  <div className={classes.datainnercolumn}>Last Name: <b>{person.lastName}</b></div>
+                </div>
+
+                <div className={classes.datainnerow}>
+                  <div className={classes.datainnercolumn}>Phone Number: <b>{person.lastName}</b></div>
+                  <div className={classes.datainnercolumn}>E-mail: <b>{person.lastName}</b></div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
