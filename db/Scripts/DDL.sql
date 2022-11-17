4b006d03cc878c26a6e8bc71a5523c58a80233e6
INT\Fernando.Giron <fernando.giron@solera.com>
2022-11-17 10:34:41 +0100

    Little fixes on the db

--- db/Scripts/DDL.sql
+++ db/Scripts/DDL.sql
@@ -1,11 +1,9 @@
 CREATE DATABASE db_person with template = template0 ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
 
-create extension pgcrypto;
-
 create table Person (
 	id uuid primary key default gen_random_uuid(),
 	firstName varchar(25) null,
-	lastName varchar(25) text null,
+	lastName varchar(25) null,
 	phoneNumber varchar(20) null,
 	email varchar(25) null
 );
